f1e48132fb8e064980d8f6a13666e8b8c02f7aa8
Nino <ninomarques.info@gmail.com>
2016-04-13 18:31:51 -0300

    api

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = restify.createServer({
 function corsHandler(req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
