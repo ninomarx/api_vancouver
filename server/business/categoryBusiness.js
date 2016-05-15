0d3f8420f359aa585ebbe6fe626cac4f3e233273
Nino <ninomarques.info@gmail.com>
2016-05-15 16:36:55 -0400

    v1.0.0.2

--- server/business/categoryBusiness.js
+++ server/business/categoryBusiness.js
@@ -14,6 +14,33 @@ var CategoryBusiness = (function() {
         var sql = "";
         sql = sql + " select * ";
         sql = sql + " from Category ";
+
+        connection.query(sql,function(err,categories){
+            connection.end();
+            if(!err) {
+
+                var collectionCategory = categories;
+
+                callback(collectionCategory);
+            }
+        });
+
+        connection.on('error', function(err) {
+            connection.end();
+            callback({"code" : 100, "status" : "Erro ao conectar com banco de dados"});
+        });
+
+
+    };
+
+    CategoryBusiness.prototype.selectFilter = function(categoryModel, callback) {
+
+        var connection = factory.getConnection();
+        connection.connect();
+
+        var sql = "";
+        sql = sql + " select * ";
+        sql = sql + " from Category ";
         sql = sql + " where cat_id IN (select distinct cat_id from course_subcategory) ";
 
 
@@ -67,6 +94,35 @@ var CategoryBusiness = (function() {
 
 
     CategoryBusiness.prototype.selectSubcategory = function(categoryModel, callback) {
+
+        var connection = factory.getConnection();
+        connection.connect();
+
+        var sql = "";
+        sql = sql + " select * ";
+        sql = sql + " from subcategory ";
+        sql = sql + " where  cat_id = " + categoryModel.cat_id;
+
+
+        connection.query(sql,function(err,categories){
+            connection.end();
+            if(!err) {
+
+                var collectionCategory = categories;
+
+                callback(collectionCategory);
+            }
+        });
+
+        connection.on('error', function(err) {
+            connection.end();
+            callback({"code" : 100, "status" : "Erro ao conectar com banco de dados"});
+        });
+
+
+    };
+
+    CategoryBusiness.prototype.selectSubcategoryFilter = function(categoryModel, callback) {
 
         var connection = factory.getConnection();
         connection.connect();
