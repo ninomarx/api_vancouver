8baf50c91ac246352cf8e7476a4170c10c92bd76
Nino <ninomarques.info@gmail.com>
2016-06-03 20:41:06 -0400

    v1.0.0.9

--- server/business/reviewBusiness.js
+++ server/business/reviewBusiness.js
@@ -16,15 +16,15 @@ var ReviewBusiness = (function() {
         sql = sql + " INSERT INTO class_review (cre_stars, cre_review, cre_added_date, cre_status, use_id, cla_id, cor_id, cre_feedback_instructor, cre_feedback_cotuto_instructor, cre_feedback_cotuto_improve) ";
         sql = sql + " VALUES( ";
         sql = sql + "  " + reviewModel.cre_stars + " , ";
-        sql = sql + "  '" + reviewModel.cre_review + "' , ";
+        sql = sql + "  '" + reviewModel.cre_review.replace(/'/g, "\\'")  + "' , ";
         sql = sql + "  now() , ";
         sql = sql + "  'A' , ";
         sql = sql + "  " + reviewModel.use_id + " , ";
         sql = sql + "  " + reviewModel.cla_id + " , ";
         sql = sql + "  " + reviewModel.cor_id + " , ";
-        sql = sql + "  '" + reviewModel.cre_feedback_instructor + "' , ";
-        sql = sql + "  '" + reviewModel.cre_feedback_cotuto_instructor + "' , ";
-        sql = sql + "  ' " + reviewModel.cre_feedback_cotuto_improve + "'   ";
+        sql = sql + "  '" + reviewModel.cre_feedback_instructor.replace(/'/g, "\\'")  + "' , ";
+        sql = sql + "  '" + reviewModel.cre_feedback_cotuto_instructor.replace(/'/g, "\\'")  + "' , ";
+        sql = sql + "  ' " + reviewModel.cre_feedback_cotuto_improve.replace(/'/g, "\\'")  + "'   ";
         sql = sql + " ) ";
 
 
