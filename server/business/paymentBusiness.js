1b6134649700e8bc50b54af52ca78b09e2621449
Nino <ninomarques.info@gmail.com>
2016-07-21 11:10:34 -0400

    v1.0.2.1

--- server/business/paymentBusiness.js
+++ server/business/paymentBusiness.js
@@ -199,7 +199,7 @@ var PaymentBusiness = (function() {
         sql = sql + " where clr_status = 'A' ";
         sql = sql + " AND clr_transaction_status = 'W' ";
         sql = sql + " ) as aux ";
-        sql = sql + " where  DATE_FORMAT(now(),'%Y-%m-%d') >  DATE_ADD(date_class, INTERVAL 3 DAY); ";
+        sql = sql + " where  DATE_FORMAT(now(),'%Y-%m-%d') >  DATE_ADD(date_class, INTERVAL 3 DAY) AND AMOUNT > 0; ";
 
 
         connection.query(sql,function(err,user_charge){
